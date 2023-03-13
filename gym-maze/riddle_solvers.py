265d6c91666a7697effaba3733435de92b68cd3d
Abdelrahman Elsayed <bodyomar79@gmail.com>
2023-03-13 20:03:42 +0200

    add captcha

--- gym-maze/riddle_solvers.py
+++ gym-maze/riddle_solvers.py
@@ -1,23 +1,13 @@
 from scapy.all import rdpcap, DNSQR, IP
 import base64
 import io
-from keras.models import load_model
-from imutils import paths
 import jwt
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.backends import default_backend
 
 
 import numpy as np
-import imutils
-import cv2
-import pickle
 import time
-
-with open("amz_captcha_model_labels.dat", "rb") as f:
-		lb = pickle.load(f)
-model = load_model("amz_captcha_model.hdf5")
-
 
 def cipher_solver(question):
     start_time = time.time()
@@ -14192,52 +14182,15 @@ image = np.array([
 
 def captcha_solver(question):
     start_time = time.time()
-    image = np.array(question)
-    # Add some extra padding around the image
-    image = cv2.copyMakeBorder(image, 20, 20, 20, 20, cv2.BORDER_REPLICATE)
-    image = cv2.convertScaleAbs(image)
-	# Sharpen the image
-    kernel = np.array([[0, -1, 0],
-					[-1, 5,-1],
-					[0, -1, 0]])
-    image = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
-
-    # threshold the image (convert it to pure black and white)
-    thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-    # find the contours (continuous blobs of pixels) the image
-    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0]
-    letter_image_regions = []
-
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-        if w / h > 1.25:
-            half_width = int(w / 2)
-            letter_image_regions.append((x, y, half_width, h))
-            letter_image_regions.append((x + half_width, y, half_width, h))
-        else:
-            letter_image_regions.append((x, y, w, h))
-
-
-    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
-
-	# Create a list to hold our predicted letters
-    captcha_text = ""
-
-    for letter_bounding_box in letter_image_regions:
-        x, y, w, h = letter_bounding_box
-        letter_image = image[y - 2:y + h + 2, x - 2:x + w + 2]
-        letter_image = resize_to_fit(letter_image, 20, 20)
-        letter_image = np.expand_dims(letter_image, axis=2)
-        letter_image = np.expand_dims(letter_image, axis=0)
-        prediction = model.predict(letter_image)
-        letter = lb.inverse_transform(prediction)[0]
-        captcha_text+=letter
-
+    img_bytes = BytesIO()
+    img = Image.fromarray(np.array(question).astype('uint8'))
+    img.save(img_bytes, format='JPEG')
+    captcha = AmazonCaptcha(img_bytes)
+    captcha.img = img
+    solution = captcha.solve()
     end_time = time.time()
     print("Time taken for captcha: ", end_time - start_time, "seconds")
-    return captcha_text
+    return solution
 
 def pcap_solver(question):
     start_time = time.time()
